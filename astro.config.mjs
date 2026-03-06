3b316b3d20cdcbc6a7959c5355d70569b873e6d6
oren0115 <nyomana369@gmail.com>
2026-03-06 13:12:53 +0700

    chore: update Astro configuration to remove Node adapter and add Vercel deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()],
     define: {
